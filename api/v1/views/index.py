bc8774bb24ae9270ddb526ada12cb56584a364a5
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2021-11-03 19:42:51 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
-"""index.py to connect to API"""
+"""
+Module for methods used by app_view blueprint
+"""
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from flasgger import swag_from
 
 
-hbnbText = {
-    "amenities": "Amenity",
-    "cities": "City",
-    "places": "Place",
-    "reviews": "Review",
-    "states": "State",
-    "users": "User"
-}
+@app_views.route('/status')
+@swag_from('swagger_spec/status.yml')
+def status():
+    """that returns a JSON: "status": 'OK'"""
+    return jsonify({'status': 'OK'})
 
 
-@app_views.route('/status', strict_slashes=False)
-def hbnbStatus():
-    """hbnbStatus"""
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def hbnbStats():
-    """hbnbStats"""
-    return_dict = {}
-    for key, value in hbnbText.items():
-        return_dict[key] = storage.count(value)
-    return jsonify(return_dict)
-
-if __name__ == "__main__":
-    pass
+@app_views.route('/stats')
+@swag_from('swagger_spec/stats.yml')
+def stats():
+    """returns a JSON with the number of each objects by type"""
+    return jsonify(
+        {
+            'amenities': storage.count(Amenity),
+            'cities': storage.count(City),
+            'places': storage.count(Place),
+            'reviews': storage.count(Review),
+            'states': storage.count(State),
+            'users': storage.count(User)
+        })
