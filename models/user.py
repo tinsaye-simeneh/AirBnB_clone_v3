8cf7dcc6f545c74c311fc33a51bf39ebc970d0f3
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2021-11-03 19:54:49 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -16,8 +17,8 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship("Place", backref="user", cascade='all, delete')
+        reviews = relationship("Review", backref="user", cascade='all, delete')
     else:
         email = ""
         password = ""
@@ -26,4 +27,11 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if kwargs:
+            if 'password' in kwargs:
+                pw = kwargs['password']
+                encoding = 'utf-8'
+                pw_hashed = hashlib.md5(pw.encode(encoding)).hexdigest()
+                kwargs.update({'password': pw_hashed})
+
         super().__init__(*args, **kwargs)
